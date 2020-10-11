acbcf93756adc2c59b4ec4824f68a0524c1b9865
german nunez <german90nunez@gmail.com>
2020-10-10 17:08:22 -0700

    fetching data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <template>
   <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..."/>
+      </div>
+
+      
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Los Angeles</div>
+          <div class="date">Friday 9th Oct 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">70 deg</div>
+          <div class="weather">Sunny</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      query: 'Los Angeles',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather(e) { 
+      if(e.key == "Enter"){
+        fetch(`${process.env.VUE_APP_URL}weather?q=${this.query}&units=metric&APPID=${process.env.VUE_APP_API}`)
+        .then(res => {
+          return res.json()
+        })
+        .then(res => {console.log(res)})
+      }
+    }
+  },
+  mounted() {
+    fetch(`${process.env.VUE_APP_URL}weather?q=${this.query}&units=metric&APPID=${process.env.VUE_APP_API}`)
+        .then(res => {
+          return res.json()
+        })
+        .then(res => {console.log(res)})
+  }
 }
 </script>
 
